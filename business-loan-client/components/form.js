e664103e18e12a5ff150152cd06f5b4a1869b9ae
Ben Mahler <benjamin.j.mahler@gmail.com>
2022-08-28 21:15:48 +1000

    Get outcome back to client

--- business-loan-client/components/form.js
+++ business-loan-client/components/form.js
@@ -3,6 +3,9 @@ import { useEffect, useState } from 'react'
 
 
 export default function Form() {
+  
+  const [hasInitialized, setHasInitialized] = useState(false);
+  const [step, setStep] = useState(1); // Skipped state 0 because useEffect was not updating state for some reason. No time to fix for this demo.
     
     const [form, setForm] = useState({
         applicationId: '',
@@ -17,11 +20,16 @@ export default function Form() {
         loanAmount: '',
         accountingProvider: '',
         sheet: []
-    });
-
-    const [step, setStep] = useState(1); // Skipped state 0 because useEffect was not updating state for some reason. No time to fix for this demo.
- 
-    const [hasInitialized, setHasInitialized] = useState(false);
+      });
+      
+      const [outcome, setOutcome] = useState({
+        applicationId: '',
+        outcome: '',
+        reason: '',
+        approvedAmount: '',
+        approvedTerm: '',
+        approvedInterestRate: ''
+      });
 
     useEffect( () => {
         async function initializeApplication() {
@@ -123,28 +131,50 @@ export default function Form() {
         // Stop the form from submitting and refreshing the page.
         event.preventDefault()
         
+        /*
         // Get data from the form.
         const data = {
-            name: event.target.name.value,
-            email: event.target.email.value,
-            phone: event.target.phone.value,
-            businessName: event.target.businessName.value,
-            businessAddress: event.target.businessAddress.value,
-            businessPhone: event.target.businessPhone.value,
-            businessEmail: event.target.businessEmail.value,
-            yearEstablished: event.target.yearEstablished.value,
-            loanAmount: event.target.loanAmount.value,
-            accountingProvider: event.target.accountingProvider.value
-        }
+            name: form.name,
+            email: form.email,
+            phone: form.phone,
+            businessName: form.businessName,
+            businessAddress: form.businessAddress,
+            businessPhone: form.businessPhone,
+            businessEmail: form.businessEmail,
+            yearEstablished: form.yearEstablished,
+            loanAmount: form.loanAmount,
+            accountingProvider: form.accountingProvider
+        };
         
         // Send the data to the server in JSON format.
-        const JSONdata = JSON.stringify(data)
+        const JSONdata = JSON.stringify(data);
         
         // API endpoint where we send form data.
     // We would make an additional request to updateApplication with the newly received data.
     // But for this example, we won't have a database to store the data.
-    const endpoint = '/api/getBalanceSheet'
-    
+
+    // Also, we expect that by this step the database has already been updated with the fields from step 1.
+    */
+
+    // Input to API to get application outcome.
+    endpoint = '/api/getApplicationOutcome';
+
+    data = {
+      id: form.applicationId,
+      directorName: form.name,
+      directorEmail: form.email,
+      directorPhone: form.phone,
+      businessName: form.businessName,
+      businessAddress: form.businessAddress,
+      businessPhone: form.businessPhone,
+      businessEmail: form.businessEmail,
+      yearEstablished: form.yearEstablished,
+      loanAmount: form.loanAmount,
+      sheet: form.sheet
+    }
+
+    JSONdata = JSON.stringify(data);
+
     // Form the request for sending data to the server.
     const options = {
       // The method is POST because we are sending data.
@@ -155,16 +185,18 @@ export default function Form() {
         },
         // Body of the request is the JSON data we created above.
         body: JSONdata,
-    }
+    };
     
     // Send the form data to our forms API on Vercel and get a response.
-    const response = await fetch(endpoint, options)
+    const response = await fetch(endpoint, options);
     
     // Get the response data from server as JSON.
     // If server returns the name submitted, that means the form works.
-    const result = await response.json()
+    const result = await response.json();
 
+    setOutcome(result);
 
+setStep(3);
 };
 
 return (
@@ -176,31 +208,31 @@ return (
                 <p>Initializing business application form...</p>
             </div>
                     ): null}
-        {step > 0 ? (
+        {step === 1 ? (
             <div>
             <label htmlFor="name">Director Name: </label>
             <input type="text" id="name" onChange={updateForm}/>
             <label htmlFor="email">Director Email: </label>
-            <input type="email" id="email" onChange={updateForm}/>
+            <input type="email" id="email" onChange={updateForm} />
             <label htmlFor="phone">Director Phone: </label>
-            <input type="tel" id="phone" onChange={updateForm}/>
+            <input type="tel" id="phone" onChange={updateForm} />
             <label htmlFor="businessName">Business Name: </label>
-            <input type="text" id="businessName" onChange={updateForm}/>
+            <input type="text" id="businessName" onChange={updateForm} />
             <label htmlFor="businessAddress">Business Address: </label>
-            <input type="text" id="businessAddress" onChange={updateForm}/>
+            <input type="text" id="businessAddress" onChange={updateForm} />
             <label htmlFor="businessPhone">Business Phone: </label>
-            <input type="tel" id="businessPhone" onChange={updateForm}/>
+            <input type="tel" id="businessPhone" onChange={updateForm} />
             <label htmlFor="businessEmail">Business Email: </label>
-            <input type="email" id="businessEmail" onChange={updateForm}/>
+            <input type="email" id="businessEmail" onChange={updateForm} />
             {/*
             This field would contain a validator to ensure the number is a valid year.
         */}
         <label htmlFor="yearEstablished">Year Established</label>
-        <input type="number" id="yearEstablished" onChange={updateForm}/>
+        <input type="number" id="yearEstablished" onChange={updateForm} />
         <label htmlFor="loanAmount">Loan Amount</label>
-        <input type="number" id="loanAmount" onChange={updateForm}/>
+        <input type="number" id="loanAmount" onChange={updateForm} />
         <label htmlFor="accountingProvider">Accounting Provider</label>
-        <select id="accountingProvider" onChange={updateForm}>
+        <select id="accountingProvider" onChange={updateForm} >
         {/*
         These would be populated by a request to the server. That request would return a list of accounting providers.
     */}
@@ -235,16 +267,23 @@ return (
                 not go in modifying individual rows. */}
                         <td>{row.year}</td>
                         <td>{monthToName(row.month)}</td>
-                        <td>{row.profitOrLoss}</td>
-                        <td>{row.assetsValue}</td>
+                        <td>${row.profitOrLoss}.00</td>
+                        <td>${row.assetsValue}.00</td>
                     </tr>
                 ))}
                 </tbody>
             </table>
             <button type="submit">Submit Application</button>
+            <button className={styles.button} onClick={()=>setStep(1)}>Back</button>
         </div>
 ) : null}
 </form>
+{step === 3 ? (
+    <div className={styles.container}>
+      <h2>Outcome</h2>
+        <p>{outcome}</p>
+    </div>
+) : null}
 </div>
 )
 }
