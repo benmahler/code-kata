63b24beb5389f8e0a1171c5f6b60b2a56b0c1f44
Ben Mahler <benjamin.j.mahler@gmail.com>
2022-08-28 19:18:14 +1000

    Implement API call in form step 1

--- business-loan-client/components/form_step_1.js
+++ business-loan-client/components/form_step_1.js
@@ -1,49 +1,75 @@
 import styles from './form.module.css'
 
 export default function FormStep1() {
+  const handleSubmit = async (event) => {
+    // Stop the form from submitting and refreshing the page.
+    event.preventDefault()
+
+    // Get data from the form.
+    const data = {
+      name: event.target.name.value,
+      email: event.target.email.value,
+      phone: event.target.phone.value,
+      businessName: event.target.businessName.value,
+      businessAddress: event.target.businessAddress.value,
+      businessPhone: event.target.businessPhone.value,
+      businessEmail: event.target.businessEmail.value,
+      yearEstablished: event.target.yearEstablished.value,
+      loanAmount: event.target.loanAmount.value,
+      accountingProvider: event.target.accountingProvider.value
+    }
+
+    // Send the data to the server in JSON format.
+    const JSONdata = JSON.stringify(data)
+
+    // API endpoint where we send form data.
+    // We would make an additional request to updateApplication with the newly received data.
+    // But for this example, we won't have a database to store the data.
+    const endpoint = '/api/getBalanceSheet'
+
+    // Form the request for sending data to the server.
+    const options = {
+      // The method is POST because we are sending data.
+      method: 'POST',
+      // Tell the server we're sending JSON.
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      // Body of the request is the JSON data we created above.
+      body: JSONdata,
+    }
+
+    // Send the form data to our forms API on Vercel and get a response.
+    const response = await fetch(endpoint, options)
+
+    // Get the response data from server as JSON.
+    // If server returns the name submitted, that means the form works.
+    const result = await response.json()
+  }
     return (
         <div className={styles.container}>
-        <form className={styles.form}>
-        <div className={styles.formGroup}>
+        <form onSubmit={handleSubmit} className={styles.form}>
           <label htmlFor="name">Director Name: </label>
           <input type="text" id="name" />
-        </div>
-        <div className={styles.formGroup}>
           <label htmlFor="email">Director Email: </label>
           <input type="email" id="email" />
-        </div>
-        <div className={styles.formGroup}>
           <label htmlFor="phone">Director Phone: </label>
           <input type="tel" id="phone" />
-        </div>
-        <div className={styles.formGroup}>
           <label htmlFor="businessName">Business Name: </label>
           <input type="text" id="businessName" />
-        </div>
-        <div className={styles.formGroup}>
           <label htmlFor="businessAddress">Business Address: </label>
           <input type="text" id="businessAddress" />
-        </div>
-        <div className={styles.formGroup}>
           <label htmlFor="businessPhone">Business Phone: </label>
           <input type="tel" id="businessPhone" />
-        </div>
-        <div className={styles.formGroup}>
           <label htmlFor="businessEmail">Business Email: </label>
           <input type="email" id="businessEmail" />
-        </div>
-        <div className={styles.formGroup}>
           {/*
               This field would contain a validator to ensure the number is a valid year.
            */}
           <label htmlFor="yearEstablished">Year Established</label>
           <input type="number" id="yearEstablished" />
-        </div>
-        <div className={styles.formGroup}>
           <label htmlFor="loanAmount">Loan Amount</label>
           <input type="number" id="loanAmount" />
-        </div>
-        <div className={styles.formGroup}>
           <label htmlFor="accountingProvider">Accounting Provider</label>
           <select id="accountingProvider">
             {/*
@@ -52,14 +78,13 @@ export default function FormStep1() {
             <option value="xero">Xero</option>
             <option value="myob">MYOB</option>
           </select>
-        </div>
-        </form>
-        {/*
+                {/*
             This button sends data to the server and invokes the next step of the form.
             It would also be a good place to add a validator to ensure the data is valid.
             Also, it would authenticate the user depending on the provider.
          */}
-        <button className={styles.button}>Load Accounts and Review</button>
+        <button className={styles.button} type="submit">Load Accounts and Review</button>
+        </form>
         </div>
     )
 }
